1d2ca3d2f0a4b9ec1444137cdf647101a8584c68
dataditie-byte <dataditie@gmail.com>
2026-07-11 16:18:15 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,31 @@
-const CACHE='mam-is-smart-x-v1';
-const FILES=['./','index.html','questions.json','manifest.json','assets/logo-mam-is.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='mam-is-smart-x-v3-1500';
+const FILES=[
+  './',
+  'index.html',
+  'manifest.json',
+  'assets/logo-mam-is.png',
+  'questions/matematika.json',
+  'questions/fisika.json'
+];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    fetch(e.request)
+      .then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
+        return res;
+      })
+      .catch(()=>caches.match(e.request))
+  );
+});
